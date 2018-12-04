787ba8f5a6221ab7320e5372baa2ab1a6441ae8d
dclkent1parker <dcl.quentind@18pixel.fr>
2018-12-04 11:02:46 +0100

    add connexion

--- Source/index.php
+++ Source/index.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <?php include('php/head.php'); ?>
     
     <title>Accueil | D codeurs du lac</title>
 </head>
---
+++ Source/php/mySQL.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "BDDFilms";
+$password = "comake";
+
+try {
+    $bdd = new PDO("mysql:host=$servername;dbname=BDDFilm", $username, $password);
+    // set the PDO error mode to exception
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+// $response = $bdd->query('select afficheFilm from Film');
+// while ($donnees = $response->fetch()){
+//     echo '<img src="'.$donnees['afficheFilm'].'/>';
+// }
+
+?>
