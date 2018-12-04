7cbe29ad89feeb550d08059c343d4619a3e2c8a2
dclkent1parker <dcl.quentind@18pixel.fr>
2018-12-04 16:58:56 +0100

    add style voir

--- Source/voir.php
+++ Source/voir.php
@@ -7,11 +7,13 @@
 <body>
     <?php include('php/header.php');?>
     <main>
-        <h1>Liste des films</h1>
-        <div class="card-deck">
+        <div id="titleVoir">
+            <h1>Liste des films</h1>
+        </div>
+        <div class="card-columns">
         <?php 
         for($x=0; $x<count($film) ; $x++){
-            echo '<div class="card"><img class="card-img-top" src="'.$film[$x]->getafficheFilm().'"/></div>';
+            echo '<div class="card" id="card'.$x.'"><img class="card-img" id="img-card'.$x.'" src="'.$film[$x]->getafficheFilm().'"/><div class="overlay" id="overlay-card'.$x.'"><div class="textOverlay">En savoir plus</div></div></div>';
         }
         ?>
         </div>
@@ -19,5 +21,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="js/bootstrap.js"></script>
+    <script src="js/voir.js"></script>
 </body>
 </html>
