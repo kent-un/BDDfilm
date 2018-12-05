1ef05e4c2103c9cfc59b97855ef3820e61423daa
dclcoconuts <dcl.corinnem@18pixel.fr>
2018-12-05 13:59:24 +0100

    ajout instance objet avec requete sql

--- Source/class/film.php
+++ Source/class/film.php
@@ -2,12 +2,30 @@
 class film{
     private $nomFilm;
     private $afficheFilm; 
-    
+    private $resumeFilm;
+    private $anneeFilm;
+    private $dureeFilm;
+    private $nomPays;
+    private $lienBandeAnnonce;
+    private $pseudoUtilisateur;
+    private $concatGenre;
+    private $concatReal;
+    private $concatAct;
 
-    public function __construct($nomFilm, $afficheFilm){
-        $this->nomFilm = $nomFilm;
-        $this->afficheFilm = $afficheFilm; 
+    public function __construct($nomFilm, $afficheFilm, $resumeFilm, $anneeFilm, $dureeFilm, $nomPays,
+    $lienBandeAnnonce, $pseudoUtilisateur, $concatGenre=NULL, $concatReal=NULL, $concatAct=NULL){
         
+        $this->nomFilm = $nomFilm;
+        $this->afficheFilm = $afficheFilm;
+        $this->resumeFilm = $resumeFilm;
+        $this->anneeFilm = $anneeFilm;
+        $this->dureeFilm = $dureeFilm;
+        $this->nomPays = $nomPays;
+        $this->lienBandeAnnonce = $lienBandeAnnonce;
+        $this->pseudoUtilisateur = $pseudoUtilisateur;
+        $this->concatGenre = $concatGenre;
+        $this->concatReal = $concatReal;
+        $this->concatAct = $concatAct;      
     }
 
     /**
@@ -16,6 +34,20 @@ class film{
     public function getAfficheFilm()
     {
         return $this->afficheFilm;
+    }
+
+    public function getInfoFilm($idFilm)
+    {
+        return $this->nomFilm;
+        return $this->resumeFilm;
+        return $this->anneeFilm;
+        return $this->dureeFilm;
+        return $this->nomPays;
+        return $this->lienBandeAnnonce;
+        return $this->pseudoUtilisateur;
+        return $this->concatGenre;
+        return $this->concatReal;
+        return $this->concatAct;  
     }
 
     /**
@@ -46,6 +78,43 @@ class film{
     public function setNomFilm($nomFilm)
     {
         $this->nomFilm = $nomFilm;
+
+        return $this;
+    }
+
+
+    /**
+     * Set the value of concatGenre
+     *
+     * @return  self
+     */ 
+    public function setConcatGenre($concatGenre)
+    {
+        $this->concatGenre = $concatGenre;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of concatReal
+     *
+     * @return  self
+     */ 
+    public function setConcatReal($concatReal)
+    {
+        $this->concatReal = $concatReal;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of concatAct
+     *
+     * @return  self
+     */ 
+    public function setConcatAct($concatAct)
+    {
+        $this->concatAct = $concatAct;
 
         return $this;
     }
